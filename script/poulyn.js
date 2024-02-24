628ff8079deb37f8527f2372e740df5ec2064891
malayaRj <156098477+malayaRj@users.noreply.github.com>
2024-02-24 16:56:15 +0800

    Create poulyn.js

---
+++ script/poulyn.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+module.exports.config = {
+  name: 'poulyn',
+  version: '1.0.0',
+  role: 0,
+  hasPrefix: false,
+  aliases: ['babes'],
+  description: "An AI command powered by GPT-4",
+  usage: "Poulyn [prompt]",
+  credits: 'Renz Cleo',
+  cooldown: 3,
+};
+module.exports.run = async function({
+  api,
+  event,
+  args
+}) {
+  const input = args.join(' ');
+  if(!input){
+    api.sendMessage(`🎀 | 𝙿𝚘𝚞𝚕𝚢𝚗 | 
+━━━━━━━━━━━━━━━━\nHello! 👋 Kumusta araw mo bhe?`, event.threadID, event.messageID);
+    api.setMessageReaction('🤨', event.messageID, () => {}, true);
+    return;
+  }
+  api.setMessageReaction('🟢', event.messageID, () => {}, true);
+  try {
+    const {
+      data
+    } = await axios.post("https://codebuddy-server.onrender.com/ai", {prompt: input});
+  const response = data.msg;
+  api.sendMessage(`🎀 | 𝙿𝚘𝚞𝚕𝚢𝚗 | 
+━━━━━━━━━━━━━━━━\n${response}`, event.threadID, event.messageID);
+    await api.setMessageReaction('✅', event.messageID, () => {}, true);
+  
+  } catch (error) {
+    api.sendMessage(`⚠️AN ERROR OCCURED IN THE CODEBUDDY SERVER WHILE FETCHING YOUR REQUEST\n\nPLEASE RETYPE YOUR QUESTIONS OR CONTACT RENZ CLEO IF THERE'S STILL A PROBLEM.`, event.threadID, event.messageID);
+  }
+ 
+};
