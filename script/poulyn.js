7e93d29fb0d21c9521cfec0551c5c177c63d4e90
CodeBuddyKingCleo <156098477+CodeBuddyKingCleo@users.noreply.github.com>
2024-03-16 11:23:03 +0800

    Update poulyn.js

--- script/poulyn.js
+++ script/poulyn.js
@@ -24,15 +24,15 @@ module.exports.run = async function({
     api.setMessageReaction('🤨', event.messageID, () => {}, true);
     return;
   }
-  api.setMessageReaction('🟢', event.messageID, () => {}, true);
+  api.setMessageReaction('🤍', event.messageID, () => {}, true);
   try {
     const {
       data
-    } = await axios.post("https://codebuddy-server.onrender.com/ai", {prompt: input, user: name});
+    } = await axios.post("https://codebuddyserver2.onrender.com/ai", {prompt: input, user: name});
   const response = data.msg;
   api.sendMessage(`🎀 | 𝙿𝚘𝚞𝚕𝚢𝚗 | 
 ━━━━━━━━━━━━━━━━\n${response}`, event.threadID, event.messageID);
-    await api.setMessageReaction('✅', event.messageID, () => {}, true);
+    await api.setMessageReaction('💚', event.messageID, () => {}, true);
   
   } catch (error) {
     api.sendMessage(`⚠️AN ERROR OCCURED IN THE CODEBUDDY SERVER WHILE FETCHING YOUR REQUEST\n\nPLEASE RETYPE YOUR QUESTIONS OR CONTACT RENZ CLEO IF THERE'S STILL A PROBLEM.`, event.threadID, event.messageID);
