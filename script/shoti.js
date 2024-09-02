a6d81fd1071bcbaf9b8189abd689eacfaad2d9a9
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-09-02 19:57:34 +0800

    Update shoti.js

--- script/shoti.js
+++ script/shoti.js
@@ -27,7 +27,7 @@ api.sendTypingIndicator(event.threadID, true);
     }
 
  try {
-  const response = await axios.post(`https://shoti-srv1.onrender.com/api/v1/get`, { apikey: `$shoti-1hg4gifgnlfdmeslom8` });
+  const response = await axios.post('http://linda.hidencloud.com:25636/shoti');
 
   const path = __dirname + `/cache files/shoti.mp4`;
   const file = fs.createWriteStream(path);
