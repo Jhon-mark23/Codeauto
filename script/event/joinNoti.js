113a88ce90a92fad9f99827a521e0072184069a4
Jhon-mark23 <123864853+Jhon-mark23@users.noreply.github.com>
2024-09-03 10:51:54 +0800

    Add files via upload

---
+++ script/event/joinNoti.js
@@ -0,0 +1,77 @@
+module.exports.config = {
+    name: "joinNoti",
+    eventType: ["log:subscribe"],
+    version: "1.0.1",
+    credits: "Anup Kumar",//MODFIED BY JOSHUA APOSTOL
+    description: "Notification of bots or people entering groups with random gif/photo/video",
+usePrefix: false,
+    dependencies: {
+        "fs-extra": "",
+        "path": "",
+        "pidusage": ""
+    }
+};
+
+module.exports.onLoad = function () {
+    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+    const { join } = global.nodemodule["path"];
+
+    const path = join(__dirname, "cache", "joinGif");
+    if (existsSync(path)) mkdirSync(path, { recursive: true }); 
+
+    const path2 = join(__dirname, "cache", "joinGif", "randomgif");
+    if (!existsSync(path2)) mkdirSync(path2, { recursive: true });
+
+    return;
+}
+
+
+module.exports.run = async function({ api, event }) {
+    const { join } = global.nodemodule["path"];
+    const { threadID } = event;
+    if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
+        api.changeNickname(`[ ${global.config.PREFIX} ] • ${(!global.config.BOTNAME) ? " " : global.config.BOTNAME}`, threadID, api.getCurrentUserID());
+        const fs = require("fs");
+        return api.sendMessage("", event.threadID, () => api.sendMessage({body: `${global.config.BOTNAME} - Bot Connected.\nMy Name is ${global.config.BOTNAME}\nMy Prefix Is [ ${global.config.PREFIX} ]\nType ${global.config.PREFIX}help to see my cmd list\nMy Owner Is ${global.config.BOTOWNER}\nUse ${global.config.PREFIX}Callad For Any Issues:\n\n::Example::\n ${global.config.PREFIX}ai ${global.config.PREFIX}pinterest ${global.config.PREFIX}sim\n${global.config.PREFIX}shoti ${global.config.PREFIX}pair ${global.config.PREFIX}random\n${global.config.PREFIX}help ${global.config.PREFIX}help all ${global.config.PREFIX}music\n `, attachment: fs.createReadStream(__dirname + "/cache/joinGif/welc.gif")} ,threadID));
+    }
+    else {
+        try {
+            const { createReadStream, existsSync, mkdirSync, readdirSync } = global.nodemodule["fs-extra"];
+            let { threadName, participantIDs } = await api.getThreadInfo(threadID);
+
+            const threadData = global.data.threadData.get(parseInt(threadID)) || {};
+            const path = join(__dirname, "cache", "joinGif");
+            const pathGif = join(path, `${threadID}.mp4`);
+
+            var mentions = [], nameArray = [], memLength = [], i = 0;
+
+            for (id in event.logMessageData.addedParticipants) {
+                const userName = event.logMessageData.addedParticipants[id].fullName;
+                nameArray.push(userName);
+                mentions.push({ tag: userName, id });
+                memLength.push(participantIDs.length - i++);
+            }
+            memLength.sort((a, b) => a - b);
+
+            (typeof threadData.customJoin == "undefined") ? msg = "Hello {name}, You're The {soThanhVien} Member of {threadName}\n\n『 Enjoy Your Stay And Make Lots Of Friends 』" : msg = threadData.customJoin;
+            msg = msg
+            .replace(/\{name}/g, nameArray.join(', '))
+            .replace(/\{type}/g, (memLength.length > 1) ?  'Friends' : 'Friend')
+            .replace(/\{soThanhVien}/g, memLength.join(', '))
+            .replace(/\{threadName}/g, threadName);
+
+            if (existsSync(path)) mkdirSync(path, { recursive: true });
+
+            const randomPath = readdirSync(join(__dirname, "cache", "joinGif", "randomgif"));
+
+            if (existsSync(pathGif)) formPush = { body: msg, attachment: createReadStream(pathGif), mentions }
+            else if (randomPath.length != 0) {
+                const pathRandom = join(__dirname, "cache", "joinGif", "randomgif", `${randomPath[Math.floor(Math.random() * randomPath.length)]}`);
+                formPush = { body: msg, attachment: createReadStream(pathRandom), mentions }
+            }
+            else formPush = { body: msg, mentions }
+
+            return api.sendMessage(formPush, threadID);
+        } catch (e) { return console.log(e) };
+    }
+                                                            }
